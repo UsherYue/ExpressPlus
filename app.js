18e885163c5262bc245ce08a745aa9770ba559ab
usher.yue <usher.yue@gmail.com>
2017-10-31 21:12:51 +0800

    add

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var path = require('path');
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var FileStreamRotator = require('file-stream-rotator');
 
 //用户模块
 var app = express();
@@ -79,6 +80,30 @@ app.use(session({
 
 //delete DEBUG_FD
 delete process.env["DEBUG_FD"];
+
+//runtime directory
+let logDirectory = path.join(__dirname, 'runtime')
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+//create runtime log files
+logger.token('param', function(req, res){
+    let list = [];
+    if(Object.is(req.method.toLowerCase(),'post') && Object.keys(req.body).length > 0){
+        for(let [k,v] of Object.entries(req.body)){
+            list = [...list,`${k}=${v}`]
+        }
+    }
+    list = list.join("&");
+    return list || '-';
+});
+let accessLogStream = FileStreamRotator.getStream({
+    date_format: 'YYYYMMDD',
+    filename: path.join(logDirectory, 'access-%DATE%.log'),
+    frequency: 'daily',
+    verbose: false
+})
+logger.format('RUNTIME', '[RUNTIME]:remote-addr - :remote-user [:date] ":method :url :param HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"');
+app.use(logger('RUNTIME', {stream: accessLogStream}));
 
 //config加载
 ({
@@ -386,6 +411,7 @@ delete process.env["DEBUG_FD"];
                 //logging:true,
                 dialectOptions: (!global.config.dbConfig.dialectOptions) ? {} : global.config.dbConfig.dialectOptions,
                 dialect: global.config.dbConfig.dbtype,
+                timezone: '+08:00',
                 replication: {
                     read: (!global.config.dbConfig.read) ? {} : global.config.dbConfig.read,
                     write: (!global.config.dbConfig.write) ? {} : global.config.dbConfig.write
@@ -515,8 +541,8 @@ delete process.env["DEBUG_FD"];
         };
         global.filterValue = (val, defaultvalue) => (typeof(val) == 'undefined' || !val) ? defaultvalue : val;
         global.return = (ret, data, msg) => ({ret: ret, data: data, msg: msg})
-        global.error = (data, msg) => ({status: 0, data: data, info: msg});
-        global.success = (data, msg) => ({status: 1, data: data, info: msg});
+        global.error = (data, msg) => ({ret: 0, data: data, msg: msg});
+        global.success = (data, msg) => ({ret: 1, data: data, msg: msg});
     },
     _initMiddleWare: function () {
         global.mw = {
--- common/sqlbuilder.js
+++ common/sqlbuilder.js
@@ -36,6 +36,7 @@ module.exports = {
         this._delete = '';
         this._set = '';
         this._orderBy = '';
+        this._where = '';
         this._limit = '';
     },
     insertInto: function (tableName, values) {
@@ -215,23 +216,36 @@ module.exports = {
         return this;
     },
     set: function () {
-        if (arguments.length == 0) {
-            console.log('miss set variables...');
-        } else if (arguments.length == 1) {
+        let argLen = arguments.length;
+        if (argLen == 1) {
             if (typeof(arguments[0]) == 'string') {
                 this._set = ' set ' + arguments[0] + ' ';
             } else {
                 let setArray = [];
-                for (var key in arguments[0]) {
-                    if (typeof(arguments[0][key]) == 'string' && arguments[0][key].search(/[\+\-\*/]/ig) != -1) {
-                        setArray.push(' `' + key + '`=' + arguments[0][key] + ' ');
+                let objArg = arguments[0];
+                for (var field in  objArg) {
+                    let fieldVal = objArg[field];
+                    if (typeof(fieldVal) == 'string') {
+                        let result = /^expr:(.+)/ig.exec(fieldVal);
+                        if (result) {
+                            setArray.push(`\`${field}\`=${result[1]}`);
+                        } else {
+                            setArray.push(`\`${field}\`='${fieldVal}'`);
+                        }
+                    } else if (Object.prototype.toString.call(fieldVal) === `[object Array]`) {
+                        //array set方法的处理
+                        if (fieldVal.length >= 3) {
+                            setArray.push(`${field}=${fieldVal[1]}${fieldVal[0]}${fieldVal[2]}`)
+                        }
                     } else {
-                        setArray.push(' `' + key + '`=\'' + arguments[0][key] + '\'');
+                        setArray.push(`\`${field}\`='${fieldVal}'`);
                     }
                 }
                 this._set = ' set ' + setArray.join(',') + ' ';
             }
-        } else {
+        } else if (argLen > 1) {
+            //保证参数是字符串数组
+            //待完善
             let setArray = Object.values(arguments);
             this._set = ' set ' + setArray.join(',') + ' ';
         }
