92cf700ada8edb92818451eadaaceec38458e9f5
usher.yue <usher.yue@gmail.com>
2022-02-22 16:58:22 +0800

    add

---
+++ common/wxpay.js
@@ -0,0 +1,17 @@
+/**
+ * ExpressPlus
+ * wxopen.js Created by usher.yue.
+ * User: usher.yue
+ * Date: 2022/2/22
+ * Time: 16:53
+ * 乘风破浪－微信支付相关工具
+ */
+
+
+"use strict";
+
+module.exports = {
+
+
+
+};
