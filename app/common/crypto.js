d04c848015e2c92fbb29c4123f320d114e790079
usher.yue <usher.yue@gmail.com>
2022-05-07 03:46:18 +0800

    增加AES DES加密支持

--- app/common/crypto.js
+++ app/common/crypto.js
@@ -7,54 +7,151 @@
 
 "use strict";
 
-var crypto = require('crypto');
-var Buffer = require('buffer').Buffer
+const crypto = require('crypto');
+const {Buffer} = require('buffer');
 
 module.exports = {
-    md5: str => {
-        let hasher = crypto.createHash('md5');
-        hasher.update(str);
-        return hasher.digest('hex').toUpperCase();
-    },
-    md5LowerCase: str => {
-        let hasher = crypto.createHash('md5');
-        hasher.update(str);
-        return hasher.digest('hex');
-    },
+    /**
+     * md5
+     * @param str
+     * @param encoding
+     * @returns {string}
+     */
+    md5: (str,encoding='hex') => crypto.createHash('md5').update(str).digest(encoding),
+    /**
+     * md5LowerCase
+     * @param str
+     * @returns {string}
+     */
+    md5LowerCase: str => this.md5(str).toLowerCase(),
+    /**
+     * md5UpperCase
+     * @param str
+     * @returns {string}
+     */
+    md5UpperCase: str => this.md5(str).toUpperCase(),
+    /**
+     * base64Encode
+     * @param str
+     * @returns {string}
+     */
     base64Encode: str => Buffer.from(str).toString('base64'),
-    base64Decode: str => new Buffer(str,'base64').toString(),
+    /**
+     * base64Decode
+     * @param str
+     * @returns {string}
+     */
+    base64Decode: str =>  Buffer.from(str,'base64').toString(),
+    /**
+     * getCiphers
+     * @returns {string[]}
+     */
+    getCiphers:()=> crypto.getCiphers(),
+    /**
+     * hash
+     * @param str
+     * @param algo
+     * @param encoding
+     * @returns {string}
+     */
     hash:(str,algo='sha256',encoding='hex')=>{
         return crypto.createHash(algo).update(str).digest(encoding);
     },
+    /**
+     * hmac
+     * @param str
+     * @param key
+     * @param algo
+     * @param encoding
+     * @returns {string}
+     */
     hmac:(str,key,algo='sha256',encoding='hex')=>{
         return crypto.createHmac('sha256', key).update(str).digest(encoding);
     },
-    rsaSign: (key, data,encode) => {
-        var sign = crypto.createSign('RSA-SHA256');
+    /**
+     * rsaSign
+     * @param key
+     * @param data
+     * @param encode
+     * @returns {string}
+     */
+    rsaSign: (key, data,encode='hex') => {
+        const sign = crypto.createSign('RSA-SHA256');
         sign.update(data);
-        var sig = sign.sign(key, encode||'hex');
+        let sig = sign.sign(key, encode||'hex');
         return sig;
     },
+    /**
+     * rsaVerify
+     * @param pubKey
+     * @param sig
+     * @param data
+     * @param encode
+     * @returns {boolean}
+     */
     rsaVerify: (pubKey, sig, data,encode) => {
-        var verify = crypto.createVerify('RSA-SHA256');
+        const verify = crypto.createVerify('RSA-SHA256');
         verify.update(data);
         return verify.verify(pubKey, sig, encode||'hex');
     },
-    desEncrypt: function (key, rawText) {
-        var keyBuf = new Buffer(key);
-        var cipher = crypto.createCipheriv('des-ecb', keyBuf, new Buffer(0));
-        cipher.setAutoPadding(true)
-        var ret = cipher.update(rawText, 'utf8', 'base64');
-        ret += cipher.final('base64');
-        return ret;
+    /**
+     * aes-128-cbc 对应16字节key iv
+     * @param key
+     * @param iv
+     * @param data
+     * @param paddingMode
+     * @returns {string}
+     */
+    aesEncrypt: function (key, iv, data,paddingMode='aes-128-cbc') {
+        const cipher = crypto.createCipheriv(paddingMode, key, iv);
+        let crypted = cipher.update(data, 'utf8', 'binary');
+        crypted += cipher.final('binary');
+        crypted = Buffer.from(crypted, 'binary').toString('base64');
+        return crypted;
     },
-    desDecrypt: function (key, encryptText) {
-        var keyBuf = new Buffer(key);
-        var decipher = crypto.createDecipheriv('des-ecb', keyBuf,new Buffer(0));
-        decipher.setAutoPadding(true);
-        var ret = decipher.update(encryptText, 'base64', 'utf8');
-        ret += decipher.final('utf8');
-        return ret;
-    }
-
+    /**
+     *aes-128-cbc 对应16字节key iv
+     * @param key
+     * @param iv
+     * @param crypted
+     * @param paddingMode
+     * @returns {string}
+     */
+    aesDecrypt: function (key, iv, crypted,paddingMode='aes-128-cbc') {
+        crypted = Buffer.from(crypted, 'base64').toString('binary');
+        const decipher = crypto.createDecipheriv(paddingMode, key, iv);
+        let decoded = decipher.update(crypted, 'binary', 'utf8');
+        decoded += decipher.final('utf8');
+        return decoded;
+    },
+    /**
+     * des-ecb  对应8字节KEY  IV
+     * @param key
+     * @param rawText
+     * @param iv
+     * @param paddingMode
+     * @returns {string}
+     */
+    desEncrypt:function (key, rawText,iv='',paddingMode='des-ecb'){
+        const cipher = crypto.createCipheriv(paddingMode, key,  Buffer.from(iv));
+        cipher.setAutoPadding(true);
+        let crypted = cipher.update(rawText, 'utf8', 'hex');
+        crypted += cipher.final('hex');
+        return crypted;
+    },
+    /**
+     * des-ecb  对应8字节KEY  IV
+     * @param key
+     * @param encryptText
+     * @param iv
+     * @param paddingMode
+     * @returns {string}
+     */
+    desDecrypt:function (key, encryptText,iv='',paddingMode='des-ecb'){
+        const cipher = crypto.createDecipheriv(paddingMode, key, Buffer.from(iv));
+        cipher.setAutoPadding(true);
+        let decrypted = cipher.update(encryptText, 'hex', 'utf8');
+        decrypted += cipher.final('utf8');
+        return decrypted;
+    },
 };
--- test/crypto.test.js
+++ test/crypto.test.js
@@ -14,4 +14,31 @@ describe('加密测试', function () {
         console.log(crypto.md5('111111'));
         done();
     });
+    it('getCinpers()获取加密算法', done => {
+        let cinphers=crypto.getCiphers();
+        console.log(cinphers)
+        done();
+    });
+    it('aes-128-cbc加密和解密', done => {
+        const key = '751f621ea5c8f930';
+        const iv = '2624750004598718';
+        const data =JSON.stringify({user: "www", pass:"wwwww/"})
+        console.log("数据加密前:",data);
+        const crypted = crypto.aesEncrypt(key, iv, data);
+        console.log("数据加密后:", crypted);
+        const dec =  crypto.aesDecrypt(key, iv, crypted);
+        console.log("数据解密后:", JSON.parse(dec));
+        done();
+    });
+    it('des-ecb加密和解密', done => {
+        const key = '01234567';
+        const iv = '';
+        let data =JSON.stringify({user: "www", pass:"wwwww/"})
+        console.log("数据加密前:",data);
+        const crypted = crypto.desEncrypt(key, data);
+        console.log("数据加密后:", crypted);
+        const dec =  crypto.desDecrypt(key,  crypted);
+        console.log("数据解密后:", JSON.parse(dec));
+        done();
+    });
 });
